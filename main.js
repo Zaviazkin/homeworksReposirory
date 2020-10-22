82030025d797b2f9eb2f784680c95523621c3d35
Roman <zavyazkin8@gmail.com>
2020-10-22 15:25:58 +0600

    fgf

---
+++ main.js
@@ -0,0 +1,22 @@
+let a = 1;
+
+while (a <= 100) {
+  console.log(a);
+  a++;
+}
+
+
+console.log('\n через for от 1 до 100\n');
+let a1 = 100;
+for (let i = 1; i <= a1; i++) {
+console.log(i);
+}
+
+
+console.log('\n через while от 11 до 33 \n');
+let b = 11;
+
+while (b <= 33) {
+  console.log(b);
+  b++;
+}
