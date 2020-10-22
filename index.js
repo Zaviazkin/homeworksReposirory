db2e5bd84c060c96669ffd94efba66d390997796
Roman <zavyazkin8@gmail.com>
2020-10-22 15:15:31 +0600

    created new files index and main

---
+++ index.js
@@ -0,0 +1,38 @@
+
+
+const word1 = "Java";
+const word2 = "ворд";
+const word3 = "конкурентно";
+const word4 = "способный";
+const word5 = "оваться";
+const word6 = "кросс";
+const word7 = "Script";
+const word8 = "поли";
+const word9 = "морфизм";
+const word10 = "ориентир";
+
+console.log("1. " + word1 + word7)
+console.log("2. " + word6 + word2)
+console.log("3. " + word3 + word4)
+console.log("4. " + word8 + word9)
+console.log("5. " + word10 + word5)
+
+
+
+let d = 7;
+let e = 2;
+
+console.log("7*2 =", d * e++)
+console.log("7*3 =",d * e++)
+console.log("7*4 =",d * e++)
+console.log("7*5 =",d * e++)
+console.log("7*6 =",d * e++)
+console.log("7*7 =",d * e++)
+console.log("7*8 =",d * e++)
+console.log("7*9 =",d * e++)
+
+
+
+let h = 21
+
+console.log("дает остаток:", h % 5)
